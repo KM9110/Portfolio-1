5c15b5713d7218e8ed00e6e33b895445ef03825b
Kunal Mandave <mandavekunal@gmail.com>
2023-11-21 13:26:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio-1
+ The Portfolio Website
---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var typed = new Typed('#element', {
+    
+    strings: ['first Line 1 first Line 1 first Line 1', 'second Line 2', 'third Line 3', 'fourth Line 4'],
+    typeSpeed: 50,
+  });
+
+  // while (true) {
+  //   var typed = new Typed('#element', {
+    
+  //     strings: ['first Line 1 first Line 1 first Line 1', 'second Line 2', 'third Line 3', 'fourth Line 4'],
+  //     typeSpeed: 50,
+  //   });
+    
+  // }
